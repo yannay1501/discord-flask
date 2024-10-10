22b90222d0b8c8e8981ce05f61569ef1a685e790
yannay <yannay1501@gmail.com>
2024-10-10 20:10:04 +0300

    test

--- .venv/test2.py
+++
@@ -1,9 +0,0 @@
-from discordwebhook import Discord
-
-discord = Discord(url="https://discordapp.com/api/webhooks/1288490670201245747/rjzj733T5IO5ITcshODT6t4nbK31vcHQmY__y_09DrssieZP6mhDItY1JSqHaIAx4LEC")
-
-discord.post(content="Testttt")
-
-
-
-
